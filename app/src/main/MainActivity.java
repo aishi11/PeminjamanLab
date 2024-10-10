22ca04253a3f87c1e8c0de969fab5df45f1b4c73
zahradivapm <130479393+zahradivapm@users.noreply.github.com>
2024-10-10 11:34:01 +0700

    Add files via upload

--- app/src/main/MainActivity.java
+++ app/src/main/MainActivity.java
@@ -1,27 +1,49 @@
-package com.example.notificationapp;
+package com.example.loginscreen;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.loginscreen.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonShowNotification;
-    private NotificationHelper notificationHelper;
+    EditText username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        notificationHelper = new NotificationHelper(this);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
 
-        buttonShowNotification = findViewById(id.buttonShowNotification);
-        buttonShowNotification.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                notificationHelper.sendNotification("New Notification", "This is a sample notification");
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
